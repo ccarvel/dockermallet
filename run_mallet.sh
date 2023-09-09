d3a27af86d5c22fed96068c590f4b27e4375a02d
Cody Carvel <cody_carvel@brown.edu>
2023-09-09 12:31:41 -0400

    Update run_mallet.sh
    test of volume mount

--- run_mallet.sh
+++ run_mallet.sh
@@ -4,4 +4,5 @@
 docker build -t mallet .
 
 # Run the Docker container
-docker run -v /${PWD}/tmp:/usr/local/mallet/malletmp -it --name malletcds mallet bash
+# docker run -v /${PWD}/tmp:/usr/local/mallet/malletmp -it --name malletcds mallet bash
+docker run -v ~/Desktop/sample-data:/sample-data -it --name malletcds mallet bash
