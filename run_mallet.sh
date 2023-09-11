57a40e80a4a5a416e1fc4dbd25a51a1d250ac278
Cody Carvel <cody_carvel@brown.edu>
2023-09-11 07:30:06 -0400

    update data to sample-data path
    now mounts $pwd/sample-data to usr/local/mallet/sample-data for local and container volume

--- run_mallet.sh
+++ run_mallet.sh
@@ -4,5 +4,5 @@
 docker build -t mallet .
 
 # Run the Docker container
-docker run -v /${PWD}/data:/usr/local/mallet/sample-data -it --name malletcds mallet bash
+docker run -v /${PWD}/sample-data:/usr/local/mallet/sample-data -it --name malletcds mallet bash
 # docker run -v ~/Desktop/sample-data:/sample-data -it --name malletcds mallet bash
